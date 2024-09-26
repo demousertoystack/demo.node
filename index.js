d611dc3184627e52400320a4d7845a0836069a43
demousertoystack <demousertoystack@gmail.com>
2024-09-26 13:19:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ var app = express();
 
 app.get('/', function (req, res) {
   res.send('Hi.............: ' + eval(req.query.q));
-  console.log(req.query.q);
 });
 
 app.listen(4000, function () {
-  console.log('port 4000');
 });
